3b8fd25a3b2b28c79f112b7093c9475acacd8738
Tsilavina007 <andriamiart114@gmail.com>
2023-12-21 13:57:12 +0300

    update cartes.js

--- interface/cartes.js
+++ interface/cartes.js
@@ -1,53 +1,68 @@
-let carteFeu = [];
-carteFeu[0] = ` ________________ `
-carteFeu[1] = `|****************|`
-carteFeu[2] = `|$     FEU      $|`
-carteFeu[3] = `|$              $|`
-carteFeu[4] = `|$              $|`
-carteFeu[5] = `|$              $|`
-carteFeu[6] = `|$      /\\      $|`
-carteFeu[7] = `|$   /\\/  \\/\\   $|`
-carteFeu[8] = `|$  (        )  $|`
-carteFeu[9] = `|$   \\      /   $|`
-carteFeu[10]= `|$    ======    $|`
-carteFeu[11]= `|$              $|`
-carteFeu[12]= `|________________|`
+function carteFeu() {
+
+    let carteFeuList = [];
+    carteFeuList[0] = ` ________________ `
+    carteFeuList[1] = `|****************|`
+    carteFeuList[2] = `|$     FEU      $|`
+    carteFeuList[3] = `|$              $|`
+    carteFeuList[4] = `|$              $|`
+    carteFeuList[5] = `|$              $|`
+    carteFeuList[6] = `|$      /\\      $|`
+    carteFeuList[7] = `|$   /\\/  \\/\\   $|`
+    carteFeuList[8] = `|$  (        )  $|`
+    carteFeuList[9] = `|$   \\      /   $|`
+    carteFeuList[10]= `|$    ======    $|`
+    carteFeuList[11]= `|$              $|`
+    carteFeuList[12]= `|________________|`
+
+    return carteFeuList
+}
 
 
-let carteEau = [];
-carteEau[0] = ` ________________ `
-carteEau[1] = `|****************|`
-carteEau[2] = `|$     EAU      $|`
-carteEau[3] = `|$              $|`
-carteEau[4] = `|$              $|`
-carteEau[5] = `|$              $|`
-carteEau[6] = `|$    ~ ~ ~ ~   $|`
-carteEau[7] = `|$   ~ ~ ~ ~ ~  $|`
-carteEau[8] = `|$    ~ ~ ~ ~   $|`
-carteEau[9] = `|$              $|`
-carteEau[10]= `|$    ======    $|`
-carteEau[11]= `|$              $|`
-carteEau[12]= `|________________|`
+function carteEau() {
+    let carteEauList = [];
+    carteEauList[0] = ` ________________ `
+    carteEauList[1] = `|****************|`
+    carteEauList[2] = `|$     EAU      $|`
+    carteEauList[3] = `|$              $|`
+    carteEauList[4] = `|$              $|`
+    carteEauList[5] = `|$              $|`
+    carteEauList[6] = `|$    ~ ~ ~ ~   $|`
+    carteEauList[7] = `|$   ~ ~ ~ ~ ~  $|`
+    carteEauList[8] = `|$    ~ ~ ~ ~   $|`
+    carteEauList[9] = `|$              $|`
+    carteEauList[10]= `|$    ======    $|`
+    carteEauList[11]= `|$              $|`
+    carteEauList[12]= `|________________|`
+
+    return carteEauList
+}
+
+function cartePlante() {
+    let cartePlanteListe = [];
+    cartePlanteListe[0] = ` ________________ `
+    cartePlanteListe[1] = `|****************|`
+    cartePlanteListe[2] = `|$    PLANTE    $|`
+    cartePlanteListe[3] = `|$              $|`
+    cartePlanteListe[4] = `|$              $|`
+    cartePlanteListe[5] = `|$     (\\/)     $|`
+    cartePlanteListe[6] = `|$   (\\(\\/)/)   $|`
+    cartePlanteListe[7] = `|$    (\\~~/)    $|`
+    cartePlanteListe[8] = `|$      ||      $|`
+    cartePlanteListe[9] = `|$      ||      $|`
+    cartePlanteListe[10]= `|$    ======    $|`
+    cartePlanteListe[11]= `|$              $|`
+    cartePlanteListe[12]= `|________________|`
+
+    return cartePlanteListe
+}
 
 
-let cartePlante = [];
-cartePlante[0] = ` ________________ `
-cartePlante[1] = `|****************|`
-cartePlante[2] = `|$    PLANTE    $|`
-cartePlante[3] = `|$              $|`
-cartePlante[4] = `|$              $|`
-cartePlante[5] = `|$     (\\/)     $|`
-cartePlante[6] = `|$   (\\(\\/)/)   $|`
-cartePlante[7] = `|$    (\\~~/)    $|`
-cartePlante[8] = `|$      ||      $|`
-cartePlante[9] = `|$      ||      $|`
-cartePlante[10]= `|$    ======    $|`
-cartePlante[11]= `|$              $|`
-cartePlante[12]= `|________________|`
 
-
-
-function lesCartes(carte1, carte2, carte3) {
+function lesCartes() {
+    let carte1 = carteEau()
+    let carte2 = carteFeu()
+    let carte3 = cartePlante()
     let cartes = []
     for (let i = 0; i < carte1.length; i++) {
         cartes.push(carte1[i]+'        '+carte2[i]+'        '+carte3[i])
@@ -58,11 +73,11 @@ function lesCartes(carte1, carte2, carte3) {
 }
 
 
-lesCartes(carteEau, carteFeu, cartePlante).forEach((element) => {console.log(element);})
-
 function carteVersus(carteUser, carteOrdi) {
     let vs = ['',`  |Votre Carte|                         |La carte de l'ordi|`]
     for (let i = 0; i < carteUser.length; i++) {vs.push(carteUser[i] + '                       '+carteOrdi[i])}
 
     return vs
 }
+
+lesCartes().forEach((element) => {console.log(element);})
